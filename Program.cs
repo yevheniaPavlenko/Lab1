a547f5fb70b83f5bb43d56133ae2d4191d013f78
yevheniaPavlenko <yevheniapavlenko@gmail.com>
2020-02-17 21:16:26 +0200

    Lab1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
-namespace Lab1
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! bletjgdfkjdgfkjdgfkd");
+            // введення вихідних даних
+            Console.Write("PI=");
+            double PI = double.Parse(Console.ReadLine());
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("x=");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("c=");пше
+            double c = double.Parse(Console.ReadLine());
+            // реалізація алгоритму розв'язання задачі
+            double y = Math.Pow((c * x - a), 3) + Math.Log(x) / 2*Math.Sin(PI / 3 + x);
+            // вивід результату
+            Console.WriteLine("y={0}", y);//
+            Console.ReadKey();//
         }
     }
 }
